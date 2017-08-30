a659946b947fe1461213da62b4f086ca368a4ee3
xy.kong <xy.kong@gmail.com>
2017-08-30 21:27:14 +0800

    Add install-pkg.sh

---
+++ install-pkg.sh
@@ -0,0 +1,32 @@
+#!/bin/sh
+
+rootpath=$1
+
+#ls -l "$rootpath"
+
+pushd "$rootpath" > /dev/null
+
+find . -type f -name '*.dmg' -exec sh -c '
+  for file do
+    echo "$file"
+ 
+    yes | hdiutil attach -mountpoint right_here "$file" > /dev/null
+
+    pushd right_here > /dev/null
+
+	sudo installer -store -pkg *.pkg -target / >/dev/null 2>&1
+
+    popd > /dev/null
+
+	until hdiutil detach right_here >/dev/null 2>&1; do
+		# echo "try again."
+		sleep 1
+	done
+
+    # diff "$file" "/some/other/path/$file"
+
+    echo "done."
+    # read line </dev/tty
+  done
+' sh {} +
+
