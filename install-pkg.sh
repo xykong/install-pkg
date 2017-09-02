16b1b6fc112c5e57b3848398917ef36684abd0bc
xy.kong <xy.kong@gmail.com>
2017-09-03 02:03:30 +0800

    Continue running if attach failed.

--- install-pkg.sh
+++ install-pkg.sh
@@ -12,16 +12,20 @@ find . -type f -name '*.dmg' -exec sh -c '
  
     yes | hdiutil attach -mountpoint right_here "$file" > /dev/null
 
+    if [ $? -eq 1 ]; then
+        continue;
+    fi
+
     pushd right_here > /dev/null
 
-	sudo installer -store -pkg *.pkg -target / >/dev/null 2>&1
+    sudo installer -store -pkg *.pkg -target / >/dev/null 2>&1
 
     popd > /dev/null
 
-	until hdiutil detach right_here >/dev/null 2>&1; do
-		# echo "try again."
-		sleep 1
-	done
+    until hdiutil detach right_here >/dev/null 2>&1; do
+        # echo "try again."
+        sleep 1
+    done
 
     # diff "$file" "/some/other/path/$file"
 
